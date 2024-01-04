0ccb9a15596fadb7a631282d81e8a72a4850019e
Martín <magadiflo@gmail.com>
2024-01-04 18:55:48 -0500

    Preparando proyecto para spring batch

---
+++ src/main/java/dev/magadiflo/billingjob/app/config/BillingJobConfig.java
@@ -0,0 +1,12 @@
+package dev.magadiflo.billingjob.app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Traducción: Billing = Facturación
+ * Esta clase será un marcador de posición para los beans relacionados con Spring Batch (Jobs, Steps, etc)
+ */
+@Configuration
+public class BillingJobConfig {
+
+}
